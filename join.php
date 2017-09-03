49d56523dd172449e7c05c886b138a114914b3ce
Amanda Ong <amandaong33@gmail.com>
2017-09-03 17:52:36 -0400

    Updated join page w/ recruitment info

--- join.php
+++ join.php
@@ -9,10 +9,32 @@ include 'header.html';
 
 <div class="content_wrapper">
 	<div class="volunteer_wrapper">
-	We are happy to welcome our new members to our team. If you are interested in joining ECaFT, our next recruitment cycle will begin in September 2017. For more details, please contact us at <a href="mailto=ecaft@cornell.edu" class="link">ecaft@cornell.edu</a>. 
-	</br>
-	<img id="group_photo" src='images/team1.jpg'/>
-	<img id="group_photo" src='images/team2.jpg'/>
+        The Cornell Engineering Career Fair Team (ECaFT) is recruiting for the 2017-2018 school year. ECaFT is an undergraduate student-run organization dedicated to planning and executing the Spring Engineering, Technical &amp; Entrepreneurial Career Fair in February. We work year-round with Cornell's Engineering Co-op &amp; Career Services Office to recruit companies from all over the nation for this unique and large-scale networking event. 
+        <br>
+        <br>
+        All students are welcome to apply! We are looking to recruit for the following sub-teams: 
+        <ul>
+            <li>Logistics</li>
+            <li>Corporate Relations</li>
+            <li>Publicity</li>
+            <li>Finance</li>
+            <li>Human Resources</li>
+            <li>Technology</li>
+        </ul>
+        Come out to ClubFest and our two information sessions to learn more about our subteams, how to apply, and for free food!
+        <ul>
+            <li>September 10th 2017 in Barton Hall</li>
+            <li>September 14th 2017 in Hollister Hall 306</li>
+            <li>September 20th 2017 in Hollister Hall 306</li>
+        </ul>
+         
+        Our applications are due on <span class="bold">September 23rd @ 11:59 PM</span> and they will be available on this page starting September 14th. 
+        <br>
+        <br>
+        And if you still have questions feel free to contact Shelley Zhang at <a href="mailto=ssz24@cornell.edu" class="link">ssz24@cornell.edu</a> or email us at <a href="mailto=ecaft@cornell.edu" class="link">ecaft@cornell.edu</a>. 
+        <br>
+        <img id="group_photo" src='images/team1.jpg'/>
+        <img id="group_photo" src='images/team2.jpg'/>
 	</div>
 </div>
 
