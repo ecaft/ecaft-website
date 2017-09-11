32aa1be948f29c58b46c460adec28ee46c420a02
Amanda Ong <amandaong33@gmail.com>
2017-09-10 21:41:48 -0400

    Added apply button

--- join.php
+++ join.php
@@ -9,6 +9,9 @@ include 'header.html';
 
 <div class="content_wrapper">
 	<div class="volunteer_wrapper">
+        <a href="https://goo.gl/forms/wnkY8eCLjGw52T783"  target="_blank" class = "link"><div class="button">APPLY</div></a>
+        <div class = "buttonSubtitle">Our applications are due on <span class="bold">September 23rd at 11:59 PM</span>.</div>
+        
         The Cornell Engineering Career Fair Team (ECaFT) is recruiting for the 2017-2018 school year. ECaFT is an undergraduate student-run organization dedicated to planning and executing the Spring Engineering, Technical &amp; Entrepreneurial Career Fair in February. We work year-round with Cornell's Engineering Co-op &amp; Career Services Office to recruit companies from all over the nation for this unique and large-scale networking event. 
         <br>
         <br>
@@ -22,17 +25,13 @@ include 'header.html';
             <li>Technology</li>
         </ul>
         Come out to ClubFest and our two information sessions to learn more about our subteams, how to apply, and for free food!
-        <ul>
+        <ul class = "list">
             <li>September 10th 2017 in Barton Hall</li>
             <li>September 14th 2017 in Hollister Hall 306</li>
             <li>September 20th 2017 in Hollister Hall 306</li>
         </ul>
-         
-        Our applications are due on <span class="bold">September 23rd @ 11:59 PM</span> and they will be available on this page starting September 14th. 
-        <br>
         <br>
         And if you still have questions feel free to contact Shelley Zhang at <a href="mailto=ssz24@cornell.edu" class="link">ssz24@cornell.edu</a> or email us at <a href="mailto=ecaft@cornell.edu" class="link">ecaft@cornell.edu</a>. 
-        <br>
         <img id="group_photo" src='images/team1.jpg'/>
         <img id="group_photo" src='images/team2.jpg'/>
 	</div>
