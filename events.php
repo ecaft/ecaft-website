f7bd5603f0bd849c834727c7fecac3341fdf0a2b
Caitlin Stanton <caitlinstanton44@gmail.com>
2017-12-04 15:37:27 -0500

    events

---
+++ events.php
@@ -0,0 +1,77 @@
+<?
+
+include 'header.html';
+
+?>
+
+<div class="title">UPCOMING EVENTS</div>
+</div>
+
+<div class="content_wrapper">
+	<div class="volunteer_wrapper">
+				<p class="team_name">February 2018 Career Fair Week Recruiting Events</p>
+				<div style="text-align: center;"><b>Connect with Cornell Engineering Talent: February 6-19, 2018</b></div>
+				<div style="text-align: center;">
+					Register now for our February career fair and join us for these other exciting events.
+				</div>
+				<br>
+				<div class="sec_title">Cornell Engineering Project Showcase</div>
+				<b>Tuesday, February 6, 5:00 - 7:00 p.m., No advance registration needed</b>
+				<div class="par">
+					Cornell Engineering students are working on creative solutions to a variety of engineering challenges. The Project Showcase is an opportunity to both see what these teams have been working on and to meet their members. From nanotechnology to water quality and from pharmaceuticals to semiconductors to robots, Cornell Engineering students are making valuable contributions to solving the challenges of our world.
+				</div>
+				<br>
+				<div class="sec_title">Undergraduate Project Team Showcase</div>
+				<b>Project Team HQ, Upson Basement</b>
+				<div class="par">
+					<a href="https://www.engineering.cornell.edu/admissions/undergraduate/why_cornell/project-teams.cfm" style="color:rgb(120, 6, 19);">Engineering Student Project Teams</a> involve nearly 1000 students, working on engineering challenges from developing an autonomous sailboat to designing safe drinking water solutions for developing countries.  These students, as well as developing quantitative skills in the classroom, are solving real and complex engineering problems, which prepare them to hit the ground running in their careers.
+				</div>
+				<br>
+				<div class="sec_title">M.Eng. Project Showcase</div>
+				<b>Duffield Atrium</b>
+				<div class="par">
+					<a href="https://www.engineering.cornell.edu/academics/graduate/degrees/meng/" style="color:rgb(120, 6, 19);">Master of Engineering (M.Eng.)</a> students engage in projects designed to provide hands-on experience solving an engineering challenge with real-world applications. Projects span the full range of engineering activities, including, for example: design; concept, prototype or product development; manufacturing or process improvements; feasibility studies; market analysis; applied or basic research; business plans; and many others. Most projects are team based and many are company sponsored to address specific industry challenges.
+				</div>
+				<br>
+				<div class="sec_title">Undergraduate Research Showcase</div>
+				<b>Duffield Atrium</b>
+				<div class="par">
+					Through undergraduate research opportunities Cornell Engineering students experience the exciting process of discovery and innovation. Almost half of our undergraduates participate in some form of research here in the college and through our affiliations with other institutions across the country and internationally.  Guided by faculty mentors, students apply classroom learning to improve existing processes or to develop entirely new approaches.
+				</div>
+				<br>
+				<div class="sec_title">Innovation Competition Showcase</div>
+				<b>Upson Lounge</b>
+				<div class="par">
+				</div>
+				<br>
+				<div class="sec_title">Cornell Society of Women Engineers Networking Dinner</div>
+				<b>Tuesday, February 6, 7:30 – 9:30 p.m.</b>
+				<div class="par">Register in advance</div>
+				<br>
+				<div class="sec_title">Technical & Entrepreneurial Career Fair</div>
+				<b>February 7, 9:00 a.m. – 2:00 p.m., Barton Hall</b>
+				<div class="par">
+					<a href="http://cornellengineeringcareerfair.com/" style="color:rgb(120, 6, 19);">Register in advance!</a>
+				</div>
+				<br>
+				<div class="sec_title">Co-op Networking Event</div>
+				<b>Wednesday, February 7, 4:30 – 6:00 p.m., Statler Ballroom</b>
+				<div class="par">
+					This is an exclusive event for employers with positions posted through our Engineering Co-op Program to network with Co-op candidates. If you are interested in participating in our Engineering Co-op Program, please contact Julie Wright @ <a href="mailto:Engr_coop@cornell.edu" style="color:rgb(120, 6, 19);">Engr_coop@cornell.edu.</a>
+				</div>
+				<br>
+				<div class="sec_title">Post-Fair Interview Schedules</div>
+				<b>Thursday, February 8, 8:00 – 4:30 p.m.</b>
+				<div class="par">
+					Limited space is available to interview students on the day following the fair. Limit of 2 rooms per employer; career fair registration required. <a href="https://ccs.career.cornell.edu/IDR/LogIn.php" style="color:rgb(120, 6, 19);">Request a room today.</a>
+				</div>
+
+
+	</div>
+</div>
+
+<?
+
+include 'footer.html';
+
+?>
